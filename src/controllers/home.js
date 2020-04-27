8bfa3af85fdf53c0f3fe09db9e0ef2b81dc5f61b
Ismaila Hassan <ismailah28@gmail.com>
2020-04-27 12:50:50 +0100

    deps: install node-postgres
    Also fixed a little eslint prettier error

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,4 @@
 import { testEnvironmentVariable } from '../settings';
 
-export const indexPage = (req, res) => {
+export const indexPage = (req, res) =>
   res.status(200).json({ message: testEnvironmentVariable });
-};
